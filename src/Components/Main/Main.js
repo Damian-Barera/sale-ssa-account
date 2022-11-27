cd4b5a0809c79e4c782aac09aeebb25a6e3262ad
Damian Barera <team@platzi.com>
2022-11-27 20:53:42 -0300

    initialiting app

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main className="main-bg">
+            <h2> Este es el main </h2>
+        </main>
+    )
+}
+
+export { Main };
