88060cb7b5434ae4943ceb26af133942a8017563
Damian Barera <team@platzi.com>
2022-11-28 01:57:36 -0300

    finish header css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Header.css';
+import imagenHeader from './imagenHeader.jpg';
 
 const Header = () => {
+
     return(
         <header className='header'>
-            <h1> Este es el Header </h1>
+            <img src={imagenHeader} alt='hyper-bright-aling' className='imagen-header' />
+            <div className='header-text'>
+                <h1> Saint Seiya Awakening Account Sale </h1>
+            </div>
+            <div className='div-ul-flex'>
+                <ul className='ul-flex'>
+                    <li><a href='#'>Main</a></li>
+                    <li><a href='#'>FAQs</a></li>
+                    <li><a href='#'>Acc Info</a></li>
+                </ul>
+            </div>
         </header>
     )
 };
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,9 +2,15 @@ import React from "react";
 import './Main.css';
 
 const Main = () => {
+
+    const principal_images = []
+
     return (
         <main className="main-bg">
-            <h2> Este es el main </h2>
+            <h2> Account Name: GD-DanteB </h2>
+            <h3> Server: A94-Shaka </h3>
+            <h3> Server Interval (A88-A94) </h3>
+            <h3> Account Price: 300 USD </h3>
         </main>
     )
 }
